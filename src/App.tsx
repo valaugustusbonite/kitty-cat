abcbd586f20c64f238412535a79384e5da94d827
val.bonite <val.bonite@whitecloak.com>
2023-04-18 22:24:19 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { DropDown } from './common/components/dropdown'
+
+function App() {
+
+  return (
+    <DropDown />
+  )
+}
+
+export default App
