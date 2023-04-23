7c7b4dd182363ea108b490a2c27df7489fe7d6f6
val.bonite <val.bonite@whitecloak.com>
2023-04-23 20:34:56 +0800

    Setup env config
    Create env file for env config

--- src/feature/cat_details/api/useGetCatDetails.ts
+++ src/feature/cat_details/api/useGetCatDetails.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
 import { QueryFunctionContext, useQuery } from 'react-query'
 import { CatDetailsInfo } from '../types'
 import { ApiException } from '@/common/exception'
+import { api } from '@/lib/axios'
 
 export const useGetCatDetails = (catId: string) => {
   return useQuery(['cat-details', catId], getCatDetails)
@@ -13,7 +13,7 @@ const getCatDetails = async ({
   try {
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
     const [_, catId] = queryKey
-    const { data } = await axios.get(`https://api.thecatapi.com/v1/images/${catId}`)
+    const { data } = await api.get(`/images/${catId}`)
 
     return data
   } catch (e: any) {
